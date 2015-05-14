a66853173d562c9b309db10ee1ca7371fcd9e6f9
emorgan21 <evan.morgan21@gmail.com>
2015-05-14 11:01:34 +0100

    Updated the neo4j config to start a webadmin for the embedded db (bound to IP address rather than localhost) and also allow neo4j shell access

--- pursuit-source-mgmt/src/main/java/org/vaadin/neo4j/Neo4JConf.java
+++ pursuit-source-mgmt/src/main/java/org/vaadin/neo4j/Neo4JConf.java
@@ -1,53 +1,42 @@
 package org.vaadin.neo4j;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import javax.annotation.PostConstruct;
+import java.net.InetAddress;
 
 import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
-import org.neo4j.kernel.impl.util.FileUtils;
+import org.neo4j.kernel.GraphDatabaseAPI;
+import org.neo4j.server.WrappingNeoServerBootstrapper;
+import org.neo4j.server.configuration.Configurator;
+import org.neo4j.server.configuration.ServerConfigurator;
+import org.neo4j.shell.ShellSettings;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.vaadin.domain.Person;
-import org.vaadin.domain.Project;
-import org.vaadin.domain.Source;
 import org.vaadin.neo4j.repository.SourceRepository;
 
 @Configuration
 @EnableNeo4jRepositories("org.vaadin.neo4j")
 @EnableTransactionManagement
-public class Neo4JConf extends Neo4jConfiguration {
+public class Neo4JConf extends Neo4jConfiguration implements CommandLineRunner {
 //	
-  private static final String DBNAME = "/home/tomcat/pursuit1.db";
-//  private static final String DBNAME = System.getProperty("user.home") + "/pursuit1.db";
+//  private static final String DBNAME = "/home/tomcat/pursuit1.db";
+  private static final String DBNAME = System.getProperty("user.home") + "/pursuit1.db";
 
     public Neo4JConf() {
         setBasePackage("org.vaadin.domain");
-//        try {
-//            // reset neo4jdb, this is a demo app...
-//            FileUtils.deleteRecursively(new File(DBNAME));
-//        	;
-//        } catch (IOException ex) {
-//            Logger.getLogger(Neo4JConf.class.getName()).log(Level.SEVERE, null,
-//                    ex);
-//        }
+
     }
 
     @Bean(destroyMethod = "shutdown")
     GraphDatabaseService graphDatabaseService() {
-        return new GraphDatabaseFactory().newEmbeddedDatabase(DBNAME);
+        return new GraphDatabaseFactory().newEmbeddedDatabaseBuilder(DBNAME)
+                .setConfig(ShellSettings.remote_shell_enabled, "true")
+                .setConfig(ShellSettings.remote_shell_port, "5555")
+                .newGraphDatabase();
     }
     
     @Bean
@@ -67,57 +56,31 @@ public class Neo4JConf extends Neo4jConfiguration {
     @Autowired
     GraphDatabaseService graphDatabase;
 
-    /**
-     * Adds some demo data to DB
-     */
-    @PostConstruct
-    public void initData() {
-        Person greg = new Person("Greg", 50, 50);
-        //greg.setAgentSourced("Greg agent");
-        Person roy = new Person("Roy", 50, 150);
-        //roy.setAgentSourced("Roy agent");
-        Person craig = new Person("Craig", 50, 250);
-        //craig.setAgentSourced("Graig agent");
-        
-        Project maintenance = new Project("Maintenance", 450, 100);
-        Project featureX = new Project("Feature 'X'", 450, 250);
 
-        Source source1 = new Source();
-        source1.setSourceName("Amedeo Adjusting");
-        Source source2 = new Source();
-        source2.setSourceName("Talisman Marketing Solutions");
-        Source source3 = new Source();
-        source3.setSourceName("Fishlake Commercial Motors Limited");
-        
-        System.out.println("Before linking up with Neo4j...");
-        for (Person person : new Person[]{greg, roy, craig}) {
-            System.out.println(person);
+    @Autowired
+    GraphDatabaseService db;
+
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void run(String... args) throws Exception {
+	       // used for Neo4j browser
+        try {
+            WrappingNeoServerBootstrapper neoServerBootstrapper;
+            GraphDatabaseAPI api = (GraphDatabaseAPI) db;
+
+            ServerConfigurator config = new ServerConfigurator(api);
+            config.configuration()
+                    .addProperty(Configurator.WEBSERVER_ADDRESS_PROPERTY_KEY, InetAddress.getLocalHost().getHostAddress());
+            config.configuration()
+                    .addProperty(Configurator.WEBSERVER_PORT_PROPERTY_KEY, "8686");
+
+            neoServerBootstrapper = new WrappingNeoServerBootstrapper(api, config);
+            neoServerBootstrapper.start();
+        } catch(Exception e) {
+            //handle appropriately
         }
-
-        try (Transaction tx = graphDatabase.beginTx()) {
-//            sourceRepository.save(source1);
-//            sourceRepository.save(source2);
-//            sourceRepository.save(source3);
-//            
-//            personRepository.save(greg);
-//            roy = personRepository.save(roy);
-//            craig = personRepository.save(craig);
-//            maintenance = projectRepository.save(maintenance);
-//            featureX = projectRepository.save(featureX);
-//            
-//            greg = personRepository.findByName(greg.getName());
-//            greg.worksIn(maintenance);
-//            greg.worksIn(featureX);
-//            personRepository.save(greg);
-//
-//            roy.worksIn(featureX);
-//            personRepository.save(roy);
-//            
-//            craig.worksIn(featureX);
-//            personRepository.save(craig);
-
-            tx.success();
-        }
-        
-    }
+        // end of Neo4j browser config
+		
+	}
 }
